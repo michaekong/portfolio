e7880999f57b6c7b3484bd9f0bdb44672800ee05
michaekong <michaelndekebai@gmail.com>
2025-01-07 13:41:34 +0100

    deploy

--- MEMOIRE/settings.py
+++ MEMOIRE/settings.py
@@ -89,14 +89,16 @@ WSGI_APPLICATION = 'MEMOIRE.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
